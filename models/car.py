383c9b81e5b30e5c1e1d206387ef7a56642141df
Zsolt Lengyel <zsolt.lengyel21@gmail.com>
2023-04-29 09:36:46 +0200

    Jefferson made me do it

--- models/car.py
+++ models/car.py
@@ -6,3 +6,14 @@ class CarModel(BaseModel):
   id = db.Column(db.Integer, primary_key=True)
   license_plate = db.Column(db.String(7))
   type = db.Column(db.String(50))
+
+  def __init__(self, license_plate, type):
+    self.license_plate = license_plate
+    self.type = type
+
+  def save_to_db(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def json(self):
+    return {'license_plate': self.license_plate, 'type': self.type}
--- resources/car.py
+++ resources/car.py
@@ -1,7 +1,23 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+from models.car import CarModel
 
 
 class Car(Resource):
+  parser = reqparse.RequestParser()
+  parser.add_argument('type',
+                      type=str,
+                      required=True,
+                      help='This field cannot be left blank')
 
   def get(self, plate):
     return {'plate': plate}
+
+  # plate is a path parameter
+  def post(self, plate):
+    # data from the request body
+    data = Car.parser.parse_args()
+    print(data)
+    car = CarModel(plate, data['type'])
+    car.save_to_db()
+
+    return car.json(), 201
